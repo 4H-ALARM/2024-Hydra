8ed6ab0e059a00eed6e8010f231e71113c97674a
Marzoman5 <aemarzoratti@gmail.com>
2024-04-09 19:36:35 -0400

    testable

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.*;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.config.DashboardConfig;
@@ -9,21 +11,26 @@ import frc.lib.config.ShooterConfig;
 public class Shooter extends SubsystemBase {
     private final CANSparkFlex shooterTopMotor;
     private final CANSparkFlex shooterBottomMotor;
+    private PIDController vPidController;
+    private final double velocitySetpoint;
+    private double pidout;
 
     public final ShooterConfig config;
     public Shooter(ShooterConfig config) {
         this.config = config;
-
         shooterTopMotor = new CANSparkFlex(this.config.shooterTopMotor, CANSparkLowLevel.MotorType.kBrushless);
 
         shooterBottomMotor = new CANSparkFlex(this.config.shooterBottomMotor, CANSparkLowLevel.MotorType.kBrushless);
         shooterBottomMotor.follow(shooterTopMotor);
 
+        vPidController.setPID(1, 0, 0);
+        velocitySetpoint = 6000;
+
 
     }
 
     public void startShooter() {
-        shooterTopMotor.set(0.5);
+        shooterTopMotor.set(pidout);
     }
 
     public void passNoteShooter() {
@@ -42,14 +49,24 @@ public class Shooter extends SubsystemBase {
     }
 
     public boolean isRevved() {
+<<<<<<< HEAD
         return false;//shooterTopMotor.getEncoder().getVelocity() > (config.targetVelocity - 200) && shooterTopMotor.getEncoder().getVelocity() < (config.targetVelocity + 200);
+=======
+        return shooterTopMotor.getEncoder().getVelocity() > (velocitySetpoint - 200) && shooterTopMotor.getEncoder().getVelocity() < (velocitySetpoint + 200);
+>>>>>>> bc6b3e2 (testable)
     }
+
 
     public boolean isShooterStopped() {
         if (shooterTopMotor.getEncoder().getVelocity() ==0) {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void periodic() {
+        pidout = vPidController.calculate(shooterTopMotor.getEncoder().getVelocity(), velocitySetpoint);
     }
 
 
